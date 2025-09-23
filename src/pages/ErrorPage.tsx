5022517fc7b566d7792b70ffb2ceea0ca12b586f
Abdullah <abdulhhany69@gmail.com>
2025-09-23 18:43:26 +0300

    react router dynamic error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,11 +1,23 @@
+import { Link, useRouteError, isRouteErrorResponse } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
-const ErrorPage = () => {
+const Error = () => {
+  const error = useRouteError();
+  let errorStatus: number;
+  let errorStatusText: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorStatus = error.status;
+    errorStatusText = error.statusText;
+  } else {
+    errorStatus = 404;
+    errorStatusText = "Page Not Found";
+  }
+
   return (
     <Container className="notFound">
-      <h1>404</h1>
-      <p>Page Not Found</p>
+      <h1>{errorStatus}</h1>
+      <p>{errorStatusText}</p>
       <Link to="/" replace={true}>
         How about going back to safety?
       </Link>
@@ -13,4 +25,4 @@ const ErrorPage = () => {
   );
 };
 
-export default ErrorPage;
+export default Error;
