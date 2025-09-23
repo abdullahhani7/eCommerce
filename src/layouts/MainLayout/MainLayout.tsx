845a4985b31f245a7a37433fb658380480b17ff0
Abdullah <abdulhhany69@gmail.com>
2025-09-23 12:13:24 +0300

    create: MainLayout & Header component

---
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import Header from "../../components/common/Header/Header";
+
+const MainLayout = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default MainLayout;
