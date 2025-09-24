6f9e1b8e96943b3a57268c4a38ab89320c6cb72d
Abdullah <abdulhhany69@gmail.com>
2025-09-24 07:20:31 +0300

    update: Render Categories List

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,34 +1,39 @@
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { actGetCategories } from "@store/categories/categoriesSlice";
+import { useEffect } from "react";
+
 import { Container, Row, Col } from "react-bootstrap";
 import { Category } from "@components/eCommerce";
 
 const Categories = () => {
+  const dispatch = useAppDispatch();
+  const { records, loading, error } = useAppSelector(
+    (state) => state.categories
+  );
+
+  useEffect(() => {
+    dispatch(actGetCategories());
+  }, [dispatch]);
+
+  const categoriesList =
+    records.length > 0
+      ? records.map((record) => (
+          <Col
+            key={record.id}
+            xs={6}
+            md={3}
+            className="d-flex justify-content-center mb-5 mt-2"
+          >
+            <Category {...record} />
+          </Col>
+        ))
+      : "there are no categories to show";
+ 
+
   return (
     <Container>
       <Row>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Category />
-        </Col>
+       {categoriesList}
       </Row>
     </Container>
   );
