ca4f116f75eaaa06747ff6c2cd460b24843cb9eb
Abdullah <abdulhhany69@gmail.com>
2025-09-28 12:47:41 +0300

    add:  Persist state with Redux Persist using Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client";
 import AppRouter from "@routes/AppRouter";
 // redux
 import { Provider } from "react-redux";
-import store from "@store/index";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@store/index";
 // styles
 import "./styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
 );
