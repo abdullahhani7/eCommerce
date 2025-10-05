4da613432ab67a3dcdf5ceb3965a732cbebb4ad0
Abdullah <abdulhhany69@gmail.com>
2025-10-05 13:55:46 +0300

    add:   type to GridList

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -5,6 +5,7 @@ import { GridList } from "@components/common";
 import { Container } from "react-bootstrap";
 import { Category } from "@components/eCommerce";
 import Loading from "@components/feedback/Loading/Loading";
+import type { TCategory } from "@customTypes/category";
 
 const Categories = () => {
   const dispatch = useAppDispatch();
@@ -21,7 +22,7 @@ const Categories = () => {
   return (
     <Container>
       <Loading loading={loading} error={error}>
-        <GridList
+        <GridList<TCategory>
           records={records}
           renderItem={(record) => <Category {...record} />}
         />
