31d2a450dfe9a546eab567f91c7920e82e32ec00
Abdullah <abdulhhany69@gmail.com>
2025-10-06 07:44:14 +0300

    add: cart item component

---
+++ src/components/eCommerce/CartItem/CartItem.tsx
@@ -0,0 +1,42 @@
+import { Form, Button } from "react-bootstrap";
+import styles from "./styles.module.css";
+
+const { cartItem, product, productImg, productInfo, cartItemSelection } =
+  styles;
+
+const CartItem = () => {
+  return (
+    <div className={cartItem}>
+      <div className={product}>
+        <div className={productImg}>
+          <img
+            src="https://media.alshaya.com/adobe/assets/urn:aaid:aem:9f660133-3497-480e-a693-24e87745e856/as/EID-414c805bab93a20c92362f7a1c20555cb933e82e.jpg?width=450&height=675&preferwebp=true"
+            alt="title"
+          />
+        </div>
+        <div className={productInfo}>
+          <h2>test</h2>
+          <h3>30 EGP</h3>
+          <Button
+            variant="secondary"
+            style={{ color: "white" }}
+            className="mt-auto"
+          >
+            Remove
+          </Button>
+        </div>
+      </div>
+
+      <div className={cartItemSelection}>
+        <span className="d-block mb-1">Quantity</span>
+        <Form.Select aria-label="Default select example">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </Form.Select>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- src/components/eCommerce/index.ts
+++ src/components/eCommerce/index.ts
@@ -1,5 +1,6 @@
 import HeaderBascket from "./HeaderBascket/HeaderBascket";
 import Product from "./Product/Product";
 import Category from "./Category/Category";
+import CartItem from "./CartItem/CartItem";
 
-export { HeaderBascket, Product, Category };
+export { HeaderBascket, Product, Category, CartItem };
