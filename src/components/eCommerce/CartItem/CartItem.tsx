783d563882f5e9b9d0572e6e2120675074c44c72
Abdullah <abdulhhany69@gmail.com>
2025-10-06 09:45:30 +0300

    render:  Cart Products Full Info

--- src/components/eCommerce/CartItem/CartItem.tsx
+++ src/components/eCommerce/CartItem/CartItem.tsx
@@ -1,25 +1,25 @@
 import { Form, Button } from "react-bootstrap";
+import type { TProduct } from "@customTypes/product";
 import styles from "./styles.module.css";
 
 const { cartItem, product, productImg, productInfo, cartItemSelection } =
   styles;
 
-const CartItem = () => {
+type CartItemProps = TProduct;
+
+const CartItem = ({ title, price, img }: CartItemProps) => {
   return (
     <div className={cartItem}>
       <div className={product}>
         <div className={productImg}>
-          <img
-            src="https://media.alshaya.com/adobe/assets/urn:aaid:aem:9f660133-3497-480e-a693-24e87745e856/as/EID-414c805bab93a20c92362f7a1c20555cb933e82e.jpg?width=450&height=675&preferwebp=true"
-            alt="title"
-          />
+          <img src={img} alt={title} />
         </div>
         <div className={productInfo}>
-          <h2>test</h2>
-          <h3>30 EGP</h3>
+          <h2>{title}</h2>
+          <h3>{price.toFixed(2)} EGP</h3>
           <Button
             variant="secondary"
-            style={{ color: "white", width:"100px" }}
+            style={{ color: "white", width: "100px" }}
             className="mt-auto"
           >
             Remove
