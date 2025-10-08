3e0dab1ebe1e0511df70ea1a27dd0f71750d6b5d
Abdullah <abdulhhany69@gmail.com>
2025-10-08 08:55:11 +0300

    build:  UI Part 1: Like/Dislike Button

--- src/components/eCommerce/Product/Product.tsx
+++ src/components/eCommerce/Product/Product.tsx
@@ -2,9 +2,12 @@ import { memo, useEffect, useState } from "react";
 import { useAppDispatch } from "@store/hooks";
 import type { TProduct } from "@customTypes/product";
 import { Button, Spinner } from "react-bootstrap";
-import styles from "./styles.module.css";
 import { addToCart } from "@store/cart/cartSlice";
-const { product, productImg, maximumNotice } = styles;
+import Like from "@assets/svg/like.svg?react";
+import LikeFill from "@assets/svg/like-fill.svg?react";
+
+import styles from "./styles.module.css";
+const { product, productImg, maximumNotice, wishlistBtn } = styles;
 
 const Product = memo(({ id, title, price, img, max, quantity }: TProduct) => {
   const dispatch = useAppDispatch();
@@ -33,6 +36,9 @@ const Product = memo(({ id, title, price, img, max, quantity }: TProduct) => {
 
   return (
     <div className={product}>
+      <div className={wishlistBtn}>
+        <Like />
+      </div>
       <div className={productImg}>
         <img src={img} alt={title} />
       </div>
