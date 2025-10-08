39b9d2fb2016b517a1c8128ad07591fd52ea08cd
Abdullah <abdulhhany69@gmail.com>
2025-10-08 09:23:42 +0300

    add : HeaderWishlist component with styles

---
+++ src/components/eCommerce/HeaderWishlist/HeaderWishlist.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Logo from "@assets/svg/wishlist.svg?react";
+
+import styles from "./styles.module.css";
+const { container, totalNum, pumpAnimate, iconWrapper } = styles;
+
+const HeaderWishlist = () => {
+  const navigate = useNavigate();
+  const [isAnimate, setIsAnimate] = useState(false);
+  const totalQuantity = 0;
+  const quantityStyle = `${totalNum} ${isAnimate ? pumpAnimate : ""}`;
+
+  useEffect(() => {
+    if (!totalQuantity) {
+      return;
+    }
+
+    setIsAnimate(true);
+
+    const debounce = setTimeout(() => {
+      setIsAnimate(false);
+    }, 300);
+
+    return () => clearTimeout(debounce);
+  }, [totalQuantity]);
+
+  return (
+    <div className={container} onClick={() => navigate("/cart")}>
+      <div className={iconWrapper}>
+        <Logo title="logo" />
+        {totalQuantity > 0 && (
+          <div className={quantityStyle}>{totalQuantity}</div>
+        )}
+      </div>
+      <h3>Wishlist</h3>
+    </div>
+  );
+};
+
+export default HeaderWishlist;
