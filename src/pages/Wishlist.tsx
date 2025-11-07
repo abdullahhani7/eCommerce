ab4ba0334c9bdcf08e1d020ef3dcd52ef4241c59
Abdullah <abdulhhany69@gmail.com>
2025-11-07 11:05:07 +0200

    Render Wishlist

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,9 +1,44 @@
-import { Heading } from "@components/common";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import {
+  actGetWishlist,
+  productsFullInfoCleanUp,
+} from "@store/wishlist/wishlistSlice";
+
+import { GridList, Heading } from "@components/common";
+import { Product } from "@components/eCommerce";
+import Loading from "@components/feedback/Loading/Loading";
+import type { TProduct } from "@customTypes/product";
 
 const Wishlist = () => {
+  const dispatch = useAppDispatch();
+  const { loading, error, productsFullInfo } = useAppSelector(
+    (state) => state.wishlist
+  );
+  const cartItems = useAppSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    dispatch(actGetWishlist());
+    return () => {
+      dispatch(productsFullInfoCleanUp());
+    };
+  }, [dispatch]);
+
+  const records = productsFullInfo.map((el) => ({
+    ...el,
+    quantity: cartItems[el.id],
+    isLiked: true,
+  }));
+
   return (
     <>
       <Heading>Your Wishlist</Heading>
+      <Loading loading={loading} error={error}>
+        <GridList<TProduct>
+          records={records}
+          renderItem={(record) => <Product {...record} />}
+        />
+      </Loading>
     </>
   );
 };
