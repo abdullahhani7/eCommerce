f6ad7db49d9c3119b09938087fb8afff99e5f36e
Abdullah <abdulhhany69@gmail.com>
2025-11-09 18:36:15 +0200

    add: custom hook for products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,10 +1,4 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "@store/hooks";
-import {
-  actGetProductsByCatPrefix,
-  cleanUpProductsRecords,
-} from "@store/products/productsSlice";
+import useProducts from "@hooks/useProducts";
 import { Container } from "react-bootstrap";
 import { Product } from "@components/eCommerce";
 import Loading from "@components/feedback/Loading/Loading";
@@ -12,29 +6,11 @@ import { GridList, Heading } from "@components/common";
 import type { TProduct } from "@customTypes/product";
 
 const Products = () => {
-  const params = useParams();
-
-  const dispatch = useAppDispatch();
-  const { records, loading, error } = useAppSelector((state) => state.products);
-  const cartItems = useAppSelector((state) => state.cart.items);
-  const wishlistItemsId = useAppSelector((state) => state.wishlist.itemsId);
-
-  useEffect(() => {
-    dispatch(actGetProductsByCatPrefix(params.prefix as string));
-    return () => {
-      dispatch(cleanUpProductsRecords());
-    };
-  }, [dispatch, params]);
-
-  const productsFullInfo = records.map((el) => ({
-    ...el,
-    quantity: cartItems[el.id] || 0,
-    isLiked: wishlistItemsId.includes(el.id),
-  }));
+  const { loading, error, productsFullInfo, productPrefix } = useProducts();
 
   return (
     <Container>
-      <Heading title={` ${params.prefix?.toUpperCase()} Products`}></Heading>
+      <Heading title={` ${productPrefix?.toUpperCase()} Products`}></Heading>
       <Loading loading={loading} error={error}>
         <GridList<TProduct>
           records={productsFullInfo}
