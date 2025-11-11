ad0bf0bbf6f9679a3002927453ca999fe967eb5e
Abdullah <abdulhhany69@gmail.com>
2025-11-11 12:19:04 +0200

    create: Register init form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Heading } from "@components/common";
+import { Col, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <> 
+      <Heading title="User Register" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" name="firstName" />
+            </Form.Group>
 
-export default Register
+            <Form.Group className="mb-3">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" name="lastName" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" name="email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" name="confirmPassword" />
+            </Form.Group>
+
+            <Button variant="info" type="submit"  style={{ color: "white" }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Register;
