b5b9528891e98ef67ab94f367941bba2e30eb23e
Abdullah <abdulhhany69@gmail.com>
2025-11-11 12:20:13 +0200

    create: Login page init form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Heading } from "@components/common";
+import { Col, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <Heading title="User Register" />
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" name="email" />
+            </Form.Group>
 
-export default Login
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" />
+            </Form.Group>
+
+            <Button variant="info" type="submit" style={{ color: "white" }}>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Login;
