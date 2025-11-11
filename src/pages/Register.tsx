ea05b21e3481b694768f22dc897bdeab3b679fcc
Abdullah <abdulhhany69@gmail.com>
2025-11-11 12:37:25 +0200

    Implement React Hook Form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,41 +1,54 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { Heading } from "@components/common";
 import { Col, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+type TFormInputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const Register = () => {
+  const { register, handleSubmit } = useForm<TFormInputs>();
+
+  const submitForm: SubmitHandler<TFormInputs> = (data) => console.log(data);
+
   return (
-    <> 
-      <Heading title="User Register" />
+    <>
+      <Heading title="User Registration" />
       <Row>
         <Col md={{ span: 6, offset: 3 }}>
-          <Form>
+          <Form onSubmit={handleSubmit(submitForm)}>
             <Form.Group className="mb-3">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" name="firstName" />
+              <Form.Control type="text" {...register("firstName")} />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" name="lastName" />
+              <Form.Control type="text" {...register("lastName")} />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="text" name="email" />
+              <Form.Control type="text" {...register("email")} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" name="password" />
+              <Form.Control type="password" {...register("password")} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" name="confirmPassword" />
+              <Form.Control type="password" {...register("confirmPassword")} />
             </Form.Group>
 
-            <Button variant="info" type="submit"  style={{ color: "white" }}>
+            <Button variant="info" type="submit" style={{ color: "white" }}>
               Submit
             </Button>
           </Form>
